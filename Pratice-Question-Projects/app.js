db5755a01a3a95cd8a75c48deb1537fad7d4297d
Abdul Rafay <99marafay@gmail.com>
2022-05-07 22:03:06 -0700

    connection between website

--- Pratice-Question-Projects/app.js
+++ Pratice-Question-Projects/app.js
@@ -1,5 +1,12 @@
 // Button click for index.html
 function take_test()
-{
-    alert("Thr connection is good!!");
+{   
+    console.log("Function being called");
+    // document.getElementId("quiz").onclick= function()
+    // {
+    //     document.location.herf="take-test.html";
+    // }
+    // window.location("../take-test.html")
+    
+    
 }
