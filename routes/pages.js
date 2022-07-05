76494787faebdf2e2c18d1a4b58f66200489d81d
Abdul Rafay <99marafay@gmail.com>
2022-07-05 17:12:12 +0500

    changes

--- routes/pages.js
+++ routes/pages.js
@@ -3,7 +3,6 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    // res.send("<h1>Hi web server is running</h1>");
     res.render("./pages/loading");
     
 });
