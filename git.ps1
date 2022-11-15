e2be217651e5d23007b7f49951897a673a0013d1
rafay99-epic <99marafay@gmail.com>
2022-11-16 01:06:50 +0500

    Updating Files in Development Branch

--- git.ps1
+++ git.ps1
@@ -12,6 +12,16 @@ if( $OutputVariable -eq "main" ) {
 {
     Write-Host("Not on master, it's the Development Branch");
     Write-Host("Running git commands");
+    
+    # Checking if the node modules are installed
+    if (Test-Path -Path "node_modules") {
+        Write-Host("Node Modules are installed");
+    } else {
+        Write-Host("Node Modules are not installed");
+        Write-Host("Installing Node Modules");
+        npm install
+    }    
+
     git status
     git add *
     git commit -m 'Updating Files in Development Branch'
